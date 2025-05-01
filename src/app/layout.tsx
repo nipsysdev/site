c5d2297a646dfac21ac60c69deb14d55319ea8c2
Xavier Saliniere <bonjour@xaviers.sh>
2025-05-01 18:43:45 -0400

    style(layout): wip base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import TerminalWindow from './components/TermWindow'
+import { PiGithubLogoFill } from 'react-icons/pi'
 
 export const metadata: Metadata = {
   title: 'xav / nipsysdev',
@@ -13,7 +15,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body id="site" className={`antialiased`}>
+        <main className="relative flex h-dvh w-dvw flex-col items-center justify-center">
+          <div className="absolute top-0 flex h-10 w-full items-center justify-between px-2 tracking-tighter transition-colors sm:px-5">
+            <div></div>
+            <a
+              className="hover:text-darkgray flex items-center text-xs lowercase md:text-sm"
+              href="https://github.com/nipsysdev/personal-website"
+              target="_blank"
+            >
+              <PiGithubLogoFill size="1.2rem" />
+              &nbsp;source code
+            </a>
+          </div>
+
+          <TerminalWindow>{children}</TerminalWindow>
+        </main>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-'use client'
-import {
-  Tabcard,
-  TabcardList,
-  TabcardTrigger,
-  TabcardContent,
-} from '@srcpunks/src_ui'
-
 export default function Home() {
-  return (
-    <main className="flex h-full items-center justify-center">
-      <Tabcard defaultValue="tab1" className="min-w-[400px]">
-        <TabcardList>
-          <TabcardTrigger value="tab1">terminal</TabcardTrigger>
-          <TabcardTrigger value="tab2">whoami</TabcardTrigger>
-          <TabcardTrigger value="tab3">blog</TabcardTrigger>
-          <TabcardTrigger value="tab4">contact</TabcardTrigger>
-        </TabcardList>
-        <TabcardContent value="tab1">Tab1 Content</TabcardContent>
-        <TabcardContent value="tab2">Tab2 Content</TabcardContent>
-        <TabcardContent value="tab3">Tab3 Content</TabcardContent>
-        <TabcardContent value="tab4">Tab4 Content</TabcardContent>
-      </Tabcard>
-    </main>
-  )
+  return <div>hello</div>
 }
