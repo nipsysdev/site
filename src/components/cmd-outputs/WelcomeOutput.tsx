d8bd0dc5a4011a3e9184ff825473b45217b81d8f
Xavier Saliniere <bonjour@xaviers.sh>
2026-05-08 20:12:03 -0400

    refactor(WelcomeOutput): convert to functional component with tooltip

--- src/components/cmd-outputs/WelcomeOutput.tsx
+++ src/components/cmd-outputs/WelcomeOutput.tsx
@@ -1,29 +1,45 @@
-import { Component } from 'react';
-import asciiArt from '@/assets/ascii-art';
+'use client';
+
+import { useStore } from '@nanostores/react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@nipsys/lsd';
+import Image from 'next/image';
+import avatar from '@/assets/nipsys.png';
+import { $scrollY } from '@/stores/terminal-store';
 import { Command, type CommandOutputProps } from '@/types/terminal';
 import CmdLink from '../terminal/CmdLink';
 
-export default class WelcomeOutput extends Component<CommandOutputProps> {
-  render() {
-    return (
-      <div className="flex flex-col">
-        <div className="mb-2 hidden font-mono text-(length:--lsd-subtitle4-fontSize) leading-none whitespace-break-spaces md:block">
-          {asciiArt}
-        </div>
+export default function WelcomeOutput({ t }: CommandOutputProps) {
+  const scrollY = useStore($scrollY);
+  const showTooltip = scrollY === 0;
 
-        <p className="mb-3">
-          {this.props.t.rich('cmds.welcome.welcome', {
-            name: (name) => <span className="font-bold">{name}</span>,
-          })}
-        </p>
+  return (
+    <TooltipProvider>
+      <div className="flex flex-col py-(--lsd-spacing-largest)">
+        <Tooltip open={showTooltip}>
+          <TooltipTrigger asChild>
+            <Image src={avatar} width={70} alt="Avatar" />
+          </TooltipTrigger>
+          <TooltipContent side="right" className="max-w-[70vw]">
+            <p className="mb-(--lsd-spacing-base)">
+              {t.rich('cmds.welcome.welcome', {
+                name: (name) => <span className="font-bold">{name}</span>,
+              })}
+            </p>
 
-        <p>{this.props.t('cmds.welcome.site_intro_1')}</p>
-        <p>
-          {this.props.t.rich('cmds.welcome.site_intro_2', {
-            cmd: () => <CmdLink cmdName={Command.Help} />,
-          })}
-        </p>
+            <p>{t('cmds.welcome.site_intro_1')}</p>
+            <p>
+              {t.rich('cmds.welcome.site_intro_2', {
+                cmd: () => <CmdLink cmdName={Command.Help} />,
+              })}
+            </p>
+          </TooltipContent>
+        </Tooltip>
       </div>
-    );
-  }
+    </TooltipProvider>
+  );
 }
