32ac847221d4004611fa6d9059d23666d23b48e8
Xavier Saliniere <bonjour@xaviers.sh>
2026-05-15 05:40:28 -0400

    chore: use prod dpulse public key

--- src/lib/dpulse/config.ts
+++ src/lib/dpulse/config.ts
@@ -1,7 +1,7 @@
 export const DPULSE_CONFIG = {
-  contentTopic: '/dpulse_site/1.0.0/dev/proto',
+  contentTopic: '/dpulse_site/1.0.0/prod/proto',
   publicKey: `-----BEGIN PUBLIC KEY-----
-MCowBQYDK2VwAyEAc/9QyIHJxRSFeXw+HvgPjKDzfCykWlr8R4sEMpBpljc=
+MCowBQYDK2VwAyEAt2CsQmm+c5L0UQmpkowqQ1WqTcFHsv6kZhI508t1sXU=
 -----END PUBLIC KEY-----`,
-  userAgent: 'dev.nipsys.dpulse-reader',
+  userAgent: 'dev.nipsys.site',
 } as const;
