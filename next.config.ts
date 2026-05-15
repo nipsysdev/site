c5cc662990fcfad3a055915221668707d6191766
Xavier Saliniere <bonjour@xaviers.sh>
2026-05-14 22:43:31 -0400

    chore: update build deps and remove outdated docs

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
