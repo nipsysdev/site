e0d07d8cfc45f2c24564ba81d42009a64b78516a
Xavier Saliniere <bonjour@xaviers.sh>
2026-05-25 20:03:04 -0400

    feat: add whoami content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, interactive-widget=resizes-content"
         />
       </head>
       <body className="h-dvh w-screen monospace">
--- src/components/layout/MainWrapper.tsx
+++ src/components/layout/MainWrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Toaster } from 'sonner';
 import Sidenav from './Sidenav';
 
 export default function MainWrapper({
@@ -7,5 +8,33 @@ export default function MainWrapper({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <Sidenav>{children}</Sidenav>;
+  return (
+    <>
+      <Sidenav>{children}</Sidenav>
+      {/* TODO: Fix toaster export in @nipsys/lsd package to avoid doing all of this */}
+      <Toaster
+        className="lsd:toaster lsd:group"
+        richColors
+        style={
+          {
+            '--normal-bg': 'var(--lsd-foreground)',
+            '--normal-text': 'var(--lsd-text-neutral)',
+            '--normal-border': 'var(--lsd-border)',
+            '--success-bg': 'var(--lsd-foreground)',
+            '--success-border': 'var(--lsd-success)',
+            '--success-text': 'var(--lsd-text-success)',
+            '--error-bg': 'var(--lsd-foreground)',
+            '--error-border': 'var(--lsd-destructive)',
+            '--error-text': 'var(--lsd-text-destructive)',
+            '--warning-bg': 'var(--lsd-foreground)',
+            '--warning-border': 'var(--lsd-warning)',
+            '--warning-text': 'var(--lsd-text-warning)',
+            '--info-bg': 'var(--lsd-foreground)',
+            '--info-border': 'var(--lsd-info)',
+            '--info-text': 'var(--lsd-text-info)',
+          } as React.CSSProperties
+        }
+      />
+    </>
+  );
 }
